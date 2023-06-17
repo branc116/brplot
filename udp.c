1c8df90e7aef35cbaebaf86e8e709931b7bed233
Branimir <branimir@ricko.us.to>
2023-06-17 17:47:34 +0200

    Bug fix.

--- udp.c
+++ udp.c
@@ -19,7 +19,7 @@ void udp_main(void* gv) {
   pthread_attr_t attrs1;
   {
     pthread_attr_init(&attrs1);
-    if (pthread_create(&thread1, &attrs1, udp_main_worker, &gv)) {
+    if (pthread_create(&thread1, &attrs1, udp_main_worker, gv)) {
       fprintf(stderr, "ERROR while creating thread %d:`%s`\n", errno, strerror(errno));
     }
   }
