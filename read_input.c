335fcb3b64cbe1966cd6d29d63ab594a096dda20
Branimir Ri\v{c}ko <rickobranimir@gmail.com>
2023-07-07 18:28:19 +0200

    Make command q bigger and write warrning when command q is full.

--- read_input.c
+++ read_input.c
@@ -48,7 +48,7 @@ static void *read_input_main_worker(void* gv) {
           .push_point_y = {
             .group = group,
             .y = value
-          } }));
+          } })) fprintf(stderr, "Q full\n");
   }
   return NULL;
 }
