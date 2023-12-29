421a3655876566ee8703f6a6d44f4a7abb4e79c4
Branimir Ri\v{c}ko <rickobranimir@gmail.com>
2023-12-29 14:54:41 +0100

    Testing webpack with brplot. Implemented test program that runs render loop from js.

--- packages/npm/src/index.js
+++ packages/npm/src/index.js
@@ -1,4 +1,18 @@
-import Module from "./brplot_imgui_release_lib.js"
+import Module from "./brplot.js"
 
-const m = Module;
-console.log(m);
+setTimeout(async () => {
+  const m = await Module({ canvas: document.getElementById("canvas") });
+  console.log(m);
+  window["brplot"] = m;
+  const graph = m._graph_malloc();
+  m._graph_init(graph, 500, 200);
+  m._graph_draw(graph);
+  window["graph"] = graph;
+  window.requestAnimationFrame(loop);
+}, 10);
+
+
+function loop() {
+  window["brplot"]._graph_draw(window["graph"]);
+  window.requestAnimationFrame(loop);
+}
