5d8b0b73e7ee8db9c6c4cf78eb7f39770f2aa343
Branimir Ri\v{c}ko <rickobranimir@gmail.com>
2024-01-06 21:24:33 +0100

    Who tf commited this this shit...

--- src/misc/shaders_dbg.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define SHADER_GRID_FS "src/desktop/shaders/grid.fs"
-#define SHADER_LINE_FS "src/desktop/shaders/line.fs"
-#define SHADER_LINE_VS "src/desktop/shaders/line.vs"
-#define SHADER_QUAD_FS "src/desktop/shaders/quad.fs"
-#define SHADER_QUAD_VS "src/desktop/shaders/quad.vs"
