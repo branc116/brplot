a347c8d47b4d95401bf5ee44401e310cae00f44b
Branimir Ri\v{c}ko <rickobranimir@gmail.com>
2024-02-18 18:46:59 +0100

    Work on 3d plot.
    There is a forward progress.
    But there is still a lot to be done..

--- src/desktop/shaders/grid_3d.fs
+++ src/desktop/shaders/grid_3d.fs
@@ -7,8 +7,10 @@ uniform vec4 resolution;
 uniform float time;
 uniform vec2 mouse;
 uniform vec2 zoom;
-uniform vec2 offset;
+uniform vec3 offset;
 uniform vec2 screen;
+
+uniform vec3 eye;
 
 uniform mat4 m_mvp;
 uniform mat4 m_view;
@@ -24,15 +26,15 @@ vec2 log10(vec2 f) {
     return log(f) / log(10.0);
 }
 
-float map(vec2 cPos, vec2 zoom_level, vec2 offset) {
+float map(vec2 cPos, vec2 zoom_level) {
     vec2 fact = log10(zoom_level * 4.5);
     vec2 fr = fract(fact);
     vec2 baseMinor = floor(fact) - 1.0;
     vec2 baseMajor = baseMinor + 1;
     vec2 divs = pow(vec2(10), baseMinor);
     vec2 divsMajor = pow(vec2(10), baseMajor);
-    cPos *= zoom_level;
-    cPos += offset;
+    //cPos *= zoom_level;
+    //cPos += offset.xy;
     vec2 mcPosd = mod(cPos + divs/2., divs) - divs/2.;
     vec2 mcPosdM = mod(cPos + divsMajor/2., divsMajor) - divsMajor/2.;
 
@@ -44,23 +46,25 @@ float map(vec2 cPos, vec2 zoom_level, vec2 offset) {
     return max(max(d.x, d.y), max(dM.x, dM.y));
 }
 
-float map_outer(vec2 fragCoord) {
-  return length(fragCoord) > 0.1 ? 1.0 : 0.0;
+vec2 map_outer(vec2 fragCoord) {
     vec2 cPos = fragCoord.xy;
     vec4 p = vec4(cPos, .0, 1.0);
     p = m_mvp * p;
-    if (p.z < 0.) return 0.0;
+    if (p.z < 0.) return vec2(0.0);
     p /= p.w;
-    if (p.z < 0.001) return 0.0;
-    return map(p.xy, vec2(1.0, 1.0), vec2(0.0, 0.0));
+    if (p.z < 0.001) return vec2(0);
+    vec3 diff = p.xyz - offset;
+    p.xyz += offset;
+    if (length(p.xy) < 0.1) return vec2(p.z, 200.0);
+    return vec2(p.z, map(p.xy, vec2(length(diff.xyz))));
 }
 
 #define AA 1
 
 void main(void) {
-  vec2 uv = (((gl_FragCoord.xy - resolution.xy)/screen) - (resolution.zw/screen)/2) / vec2(resolution.w/resolution.z, 1.0);
-  //uv /= vec2(resolution.w/resolution.z, 1.0);
-  //uv -= vec2(resolution.z/screen.x/2,  -resolution.w/screen.y/2);
-  float res = map_outer(uv);
-  out_color = vec4(0.2, 0.3, 0.5, 1.0)*res;
+  vec2 uv = (gl_FragCoord.xy - resolution.xy - resolution.zw*0.5)/screen.yy;
+  vec2 res = map_outer(uv);
+  //out_color = vec4(0.2, 0.3, 0.5, 1.0)*res;
+  out_color = vec4(res.x/100., res.y, 0., 1.0);
 }
+
--- src/imgui/hot.cpp
+++ src/imgui/hot.cpp
@@ -18,8 +18,11 @@ void ShowMatrix(const char* name, Matrix m) {
   ImGui::PopID();
 }
 
+
+static Vector3 translate;
 extern "C" void br_hot_init(br_plot_t* gv) {
   fprintf(stderr, "First call\n");
+  translate = {0,0,1};
 }
 
 Vector3 Vector3MultiplyValue(Vector3 v, float s) {
@@ -30,6 +33,14 @@ extern "C" void br_hot_loop(br_plot_t* gv) {
   ImGui::Begin("3d");
   br::Input("Eyes", gv->eye);
   br::Input("Target", gv->target);
+  br::Input("Up", gv->up);
+  br::Input("Trans", translate);
+  if (ImGui::Button("Reset") || IsKeyPressed(KEY_R)) {
+    gv->eye = Vector3 { 0, 0, -1 };
+    gv->target = Vector3 { 0, 0, 0 };
+    gv->up = Vector3 { 0, 1, 0 };
+    translate = Vector3 { 0, 0, 0 };
+  }
   ImGui::InputFloat("FovY", &gv->fov_y);
   ImGui::InputFloat("Near", &gv->near_plane);
   ImGui::InputFloat("Far", &gv->far_plane);
@@ -39,33 +50,83 @@ extern "C" void br_hot_loop(br_plot_t* gv) {
   ShowMatrix("Rl Project", rlGetMatrixProjection());
   ShowMatrix("Rl ModelView", rlGetMatrixTransform());
   if (IsKeyDown(KEY_UP)) {
-     Vector3 diff = Vector3MultiplyValue(Vector3Subtract(gv->target, gv->eye), 0.01f);
-     gv->target = Vector3Add(gv->target, diff);
-     gv->eye = Vector3Add(gv->eye, diff);
+     Vector3 diff = Vector3MultiplyValue(gv->up, 0.01f);
+     translate = Vector3Add(translate, diff);
+     //gv->target = Vector3Add(gv->target, diff);
+     //gv->eye = Vector3Add(gv->eye, diff);
   }
 
   if (IsKeyDown(KEY_DOWN)) {
-     Vector3 diff = Vector3MultiplyValue(Vector3Subtract(gv->target, gv->eye), -0.01f);
-     gv->target = Vector3Add(gv->target, diff);
-     gv->eye = Vector3Add(gv->eye, diff);
+     Vector3 diff = Vector3MultiplyValue(gv->up, -0.01f);
+     translate = Vector3Add(translate, diff);
+     //gv->target = Vector3Add(gv->target, diff);
+     //gv->eye = Vector3Add(gv->eye, diff);
+  }
+
+  if (IsKeyDown(KEY_LEFT)) {
+     Vector3 diff = Vector3MultiplyValue(Vector3CrossProduct(Vector3Subtract(gv->target, gv->eye), gv->up), 0.01f);
+     translate = Vector3Add(translate, diff);
+     //gv->target = Vector3Add(gv->target, diff);
+     //gv->eye = Vector3Add(gv->eye, diff);
+  }
+
+  if (IsKeyDown(KEY_RIGHT)) {
+     Vector3 diff = Vector3MultiplyValue(Vector3CrossProduct(Vector3Subtract(gv->target, gv->eye), gv->up), -0.01f);
+     translate = Vector3Add(translate, diff);
+     //gv->target = Vector3Add(gv->target, diff);
+     //gv->eye = Vector3Add(gv->eye, diff);
+  }
+
+  if (IsKeyDown(KEY_J)) {
+    Vector3 diff = Vector3Subtract(gv->target, gv->eye);
+    Vector3 right = Vector3Normalize(Vector3CrossProduct(diff, gv->up));
+    gv->target = Vector3Add(Vector3RotateByAxisAngle(diff, right, 0.01f), gv->eye);
+    gv->up = Vector3RotateByAxisAngle(gv->up, right, 0.01f);
+  }
+
+  if (IsKeyDown(KEY_K)) {
+    Vector3 diff = Vector3Subtract(gv->target, gv->eye);
+    Vector3 right = Vector3Normalize(Vector3CrossProduct(diff, gv->up));
+    gv->target = Vector3Add(Vector3RotateByAxisAngle(diff, right, -0.01f), gv->eye);
+    gv->up = Vector3RotateByAxisAngle(gv->up, right, -0.01f);
+  }
+  
+  if (IsKeyDown(KEY_L)) {
+    Vector3 diff = Vector3Subtract(gv->target, gv->eye);
+    gv->target = Vector3Add(Vector3RotateByAxisAngle(diff, Vector3 { 0.f, 1.f, 0.f }, 0.01f), gv->eye);
+  }
+
+  if (IsKeyDown(KEY_H)) {
+    Vector3 diff = Vector3Subtract(gv->target, gv->eye);
+    gv->target = Vector3Add(Vector3RotateByAxisAngle(diff, Vector3 { 0.f, 1.f, 0.f }, -0.01f), gv->eye);
   }
 
   //rlSetUniformMatrix(gv->grid3dShader.uMatProjection, gv->uvMatProjection);
 
   ImGui::End();
   ImGui::Begin("3d view");
-  glUseProgram(gv->grid3dShader.shader.id);
-  ImVec2 v = ImGui::GetWindowPos();
-  ImVec2 size = ImGui::GetWindowSize();
-  Vector2 screen = { (float)GetScreenWidth(), (float)GetScreenHeight() };
-  Rectangle r = { .x = v.x / screen.x * 2 - 1, .y = ((screen.y - v.y)*2.f / screen.y) - 1.5f, .width = 2*size.x / screen.x, .height = 2*size.y / screen.y };
-  Rectangle r1 = { .x = v.x, .y = GetScreenHeight() - v.y , .width = size.x, .height = -size.y };
-  rlSetUniform(gv->grid3dShader.uResolution, &r1, RL_SHADER_UNIFORM_VEC4, 1);
-  rlSetUniform(gv->grid3dShader.uScreen, &screen, RL_SHADER_UNIFORM_VEC2, 1);
-  
-  smol_mesh_gen_quad_simple(gv->graph_mesh_3d, r, RED);
-  smol_mesh_update(gv->graph_mesh_3d);
-  smol_mesh_draw(gv->graph_mesh_3d);
+    glUseProgram(gv->grid3dShader.shader.id);
+    ImVec2 v = ImGui::GetWindowPos();
+    ImVec2 size = ImGui::GetWindowSize();
+    Vector2 screen = { (float)GetRenderWidth(), (float)GetRenderHeight() };
+    Rectangle r = { .x = v.x / screen.x * 2 - 1, .y = 1.f - 2.f*(v.y + size.y) / screen.y, .width = 2 * size.x / screen.x, .height = 2 * size.y / screen.y };
+    Rectangle r1 = { .x = v.x, .y = GetScreenHeight() - v.y , .width = size.x, .height = -size.y };
+
+    int loc = GetShaderLocation(gv->grid3dShader.shader, "eye");
+    rlSetUniform(gv->grid3dShader.uResolution, &r1, RL_SHADER_UNIFORM_VEC4, 1);
+    rlSetUniform(gv->grid3dShader.uScreen, &screen, RL_SHADER_UNIFORM_VEC2, 1);
+    rlSetUniform(gv->grid3dShader.uOffset, &translate, RL_SHADER_UNIFORM_VEC3, 1);
+    rlSetUniform(loc, &gv->eye, RL_SHADER_UNIFORM_VEC3, 1);
+
+    gv->uvMatProjection = MatrixPerspective(gv->fov_y, 1.0f, gv->near_plane, gv->far_plane);
+    gv->uvMatView = MatrixLookAt(gv->eye, gv->target, gv->up);
+    Matrix transMatrix = MatrixTranslate(translate.x, translate.y, translate.z);
+    gv->uvMvp = MatrixMultiply(gv->uvMatView, gv->uvMatProjection);
+    rlSetUniformMatrix(gv->grid3dShader.uMvp, gv->uvMvp);
+    
+    smol_mesh_gen_quad_simple(gv->graph_mesh_3d, r, RED);
+    smol_mesh_update(gv->graph_mesh_3d);
+    smol_mesh_draw(gv->graph_mesh_3d);
   ImGui::End();
 }
 
