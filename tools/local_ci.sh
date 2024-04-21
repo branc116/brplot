70275a1d4f105bb7ee6d5dcbbddf1b28e59b3324
Branimir Ri\v{c}ko <rickobranimir@gmail.com>
2024-04-21 12:50:41 +0200

    Limit data read during fuzz test.

--- tools/local_ci.sh
+++ tools/local_ci.sh
@@ -32,11 +32,11 @@ make $@ PLATFORM=WEB                  CONFIG=DEBUG   GUI=HEADLESS  TYPE=LIB && \
 make $@ PLATFORM=WEB                  CONFIG=RELEASE GUI=IMGUI     TYPE=LIB && \
 make $@ PLATFORM=WEB                  CONFIG=RELEASE GUI=RAYLIB    TYPE=LIB && \
 make $@ PLATFORM=WEB                  CONFIG=RELEASE GUI=HEADLESS  TYPE=LIB && \
-echo "Running fuzztests" && time cat /dev/random | ./bin/brplot_headless_linux_debug_gcc > /dev/null && echo "Fuzz test OK" && \
+echo "Running fuzztests" && time cat /dev/random | head --bytes=1M ./bin/brplot_headless_linux_debug_gcc > /dev/null && echo "Fuzz test OK" && \
 time ./bin/brplot_imgui_linux_debug_gcc --unittest && echo "UNIT TESTS for imgui OK" && \
 time ./bin/brplot_raylib_linux_debug_gcc --unittest && echo "UNIT TESTS for raylib OK" && \
 time ./bin/brplot_headless_linux_debug_gcc --unittest && echo "UNIT TESTS for headless OK" && \
-echo "Running fuzztests" && time cat /dev/random | ./bin/brplot_headless_linux_debug_clang > /dev/null && echo "Fuzz test OK" && \
+echo "Running fuzztests" && time cat /dev/random | head --bytes=1M | ./bin/brplot_headless_linux_debug_clang > /dev/null && echo "Fuzz test OK" && \
 time ./bin/brplot_imgui_linux_debug_clang --unittest && echo "UNIT TESTS for imgui OK" && \
 time ./bin/brplot_raylib_linux_debug_clang --unittest && echo "UNIT TESTS for raylib OK" && \
 time ./bin/brplot_headless_linux_debug_clang --unittest && echo "UNIT TESTS for headless OK" && \
