f1fa128327cbf9cd85034d539370b534a336c907
Branimir Ri\v{c}ko <rickobranimir@gmail.com>
2024-10-13 23:28:31 +0200

    Debug vimrc.

--- vimrc.lua
+++ vimrc.lua
@@ -20,3 +20,21 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.nu = true
 vim.opt.relativenumber = true
+
+local dap = require('dap')
+
+vim.keymap.set("n", "<F5>", function()
+  dap.list_breakpoints()
+  local file = io.open("BPS", "w");
+  if file == nil then
+    print("Faile to open a file")
+    return
+  end
+  for _, ql in ipairs(vim.fn.getqflist()) do
+    local s = string.format("%s:%d\n", vim.fn.getbufinfo(ql.bufnr)[1].name, ql.lnum);
+    file:write(s)
+  end
+  file:close()
+  vim.cmd[[silent !gf2 --command=tools/gdb_debug.py ./brplot &]]
+end)
+
