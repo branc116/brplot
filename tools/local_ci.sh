3e7781142136c7c1c7e4219b6b9bb10d7afb9dac
Branimir Ri\v{c}ko <rickobranimir@gmail.com>
2025-07-14 17:21:05 +0200

    CI.

--- tools/local_ci.sh
+++ tools/local_ci.sh
@@ -1,37 +1,19 @@
 #!/usr/bin/env sh
 
 set -ex
-for CONFIG in DEBUG RELEASE
+for TYPE in EXE
 do
-  for TYPE in LIB EXE
-  do
-    make $@ PLATFORM=WEB CONFIG=$CONFIG HEADLESS=NO TYPE=$TYPE
-  done
+  make $@ PLATFORM=WEB HEADLESS=NO TYPE=$TYPE
 done
 
-for CONFIG in DEBUG RELEASE
+for TYPE in EXE
 do
-  for HEADLESS in YES NO
-  do
-    for TYPE in LIB EXE
-    do
-      make $@ PLATFORM=WINDOWS CONFIG=$CONFIG HEADLESS=$HEADLESS TYPE=$TYPE
-    done
-  done
+  make $@ PLATFORM=WINDOWS TYPE=$TYPE
 done
 
-for CONFIG in DEBUG RELEASE
+for TYPE in LIB EXE
 do
-  for COMPILER in GCC CLANG ZIG
-  do
-    for HEADLESS in YES NO
-    do
-      for TYPE in LIB EXE
-      do
-        make $@ PLATFORM=LINUX COMPILER=$COMPILER CONFIG=$CONFIG HEADLESS=$HEADLESS TYPE=$TYPE
-      done
-    done
-  done
+  make $@ PLATFORM=LINUX TYPE=$TYPE
 done
 
 cc nob.c -O2 -I. -lm -o nob
