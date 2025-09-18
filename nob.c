6bef63a36abc280df1054ad5575ad26d1c9fa6d5
Branimir Ri\v{c}ko <rickobranimir@gmail.com>
2025-09-18 17:07:39 +0200

    Removed mini_shell.html.

--- nob.c
+++ nob.c
@@ -420,7 +420,7 @@ static bool compile_and_link(Nob_Cmd* cmd) {
   }
   if (is_wasm) {
     nob_cmd_append(&link_command, "-sWASM_BIGINT", "-sALLOW_MEMORY_GROWTH", "-sUSE_GLFW=3", "-sUSE_WEBGL2=1",
-        "-sGL_ENABLE_GET_PROC_ADDRESS", "--shell-file=src/web/minshell.html",
+        "-sGL_ENABLE_GET_PROC_ADDRESS",
         "-sCHECK_NULL_WRITES=0", "-sDISABLE_EXCEPTION_THROWING=1", "-sFILESYSTEM=0", "-sDYNAMIC_EXECUTION=0");
     if (is_lib) {
       nob_cmd_append(&link_command, "-sMODULARIZE=1", "-sEXPORT_ES6=1");
--- tools/clang_cross.sh
+++ tools/clang_cross.sh
@@ -5,4 +5,4 @@ clang -I. ./tests/hello_world.c -lm
 clang -I. -target x86_64-pc-win32-gnu ./tests/hello_world.c -lgdi32
 clang -I. -target x86_64-pc-win32-gnu ./tests/hello_world.c -lgdi32
 clang -I. -target x86_64-apple-darwin ./tests/hello_world.c -lgdi32
-emcc -sWASM_BIGINT -sALLOW_MEMORY_GROWTH -sUSE_GLFW=3 -sUSE_WEBGL2=1 -sGL_ENABLE_GET_PROC_ADDRESS --shell-file=src/web/minshell.html -sCHECK_NULL_WRITES=0 -sDISABLE_EXCEPTION_THROWING=1 -sFILESYSTEM=0 -sDYNAMIC_EXECUTION=0 -DBRPLOT_IMPLEMENTATION .generated/brplot.c
+emcc -sWASM_BIGINT -sALLOW_MEMORY_GROWTH -sUSE_GLFW=3 -sUSE_WEBGL2=1 -sGL_ENABLE_GET_PROC_ADDRESS -sCHECK_NULL_WRITES=0 -sDISABLE_EXCEPTION_THROWING=1 -sFILESYSTEM=0 -sDYNAMIC_EXECUTION=0 -DBRPLOT_IMPLEMENTATION .generated/brplot.c
